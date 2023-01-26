27a5520aa7254fd5caba555d9e61f25082f4b739
Gustavo Costa <32878844+costaguh@users.noreply.github.com>
2023-01-26 12:10:13 -0300

    Search

--- lwc/faq.js
+++ lwc/faq.js
@@ -3,21 +3,32 @@ import { LightningElement, track } from 'lwc';
 import noHeader from '@salesforce/resourceUrl/NoHeader';
 import {loadStyle} from "lightning/platformResourceLoader";
 import getFaqs from "@salesforce/apex/FaqsController.getFaqs";
-
  
 export default class Faq extends LightningElement {
-    @track data = [];    
+    @track data = [];
+    error;
+    searchWords = '';    
 
     connectedCallback() {
-        getFaqs()
-            .then(result =>{
-                this.data = result;                
-            })
-            .catch(error => {
-                console.log('erro');
-            })
+        this.loadFaqs(this.searchWords);
 
         loadStyle(this, noHeader)
             .then(result => {});
     }
+
+    handleSearch(event){
+        this.searchWords = event.target.value;
+        this.loadFaqs(this.searchWords);
+    }
+
+    loadFaqs(searchWords){
+        getFaqs({ searchKey: searchWords})
+            .then(result =>{
+                this.data = result;                
+            })
+            .catch(error => {
+                this.error = error;
+            })
+    }
+    
 }
